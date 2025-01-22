bee9ed5f31f6ab97a39eb0e89766564afebbb7b0
Codey Huntting <creativecodey@gmail.com>
2025-01-22 08:50:13 -0600

    working on extracting capsule drawing code

---
+++ Assets/Package/Scripts/Runtime/Gizmos/CapsuleGizmo.cs
@@ -0,0 +1,7 @@
+namespace PhysicsQuery
+{
+    public static class CapsuleGizmo
+    {
+
+    }
+}
--- Assets/Package/Scripts/Runtime/Gizmos/GizmoShape_Capsule.cs
+++ Assets/Package/Scripts/Runtime/Gizmos/GizmoShape_Capsule.cs
@@ -18,57 +18,61 @@ namespace PhysicsQuery
         protected override void DrawShape(Vector3 center, Color color)
         {
             Vector3 up = Query.GetWorldAxis();
-            Vector3 cap1 = center + up;
-            Vector3 cap2 = center - up;
-            Vector3 direction = GetWorldRay().direction;
-            Vector3 crossAxis = Colinear(up, direction) ? GetArbitraryCrossAxis(up) : direction;
-            Vector3 right = Vector3.Cross(up, crossAxis).normalized * Query.Radius;
-            Vector3 forward = Vector3.Cross(up, right).normalized * Query.Radius;
+            float radius = Query.Radius;
+            Gizmos.color = color;
+            DrawCapsule(center, up, radius);
+        }
+
+        private static void DrawCapsule(Vector3 center, Vector3 up, float radius)
+        {
+            Vector3 topCapCenter = center + up;
+            Vector3 bottomCapCenter = center - up;
+            Vector3 crossAxis = GetArbitraryCrossAxis(up);
+            Vector3 right = Vector3.Cross(up, crossAxis).normalized * radius;
+            Vector3 forward = Vector3.Cross(up, right).normalized * radius;
 
             Vector3 left = -right;
             Vector3 down = -up;
             Vector3 back = -forward;
 
-            Gizmos.color = color;
-            DrawHemisphere(cap1, right, up, forward);
-            DrawHemisphere(cap2, right, down, forward);
-            DrawLine(center + forward + up, center + forward + down, color);
-            DrawLine(center + back + up, center + back + down, color);
-            DrawLine(center + right + up, center + right + down, color);
-            DrawLine(center + left + up, center + left + down, color);
+            DrawHemisphere(topCapCenter, right, up, forward, radius);
+            DrawHemisphere(bottomCapCenter, right, down, forward, radius);
+            Gizmos.DrawLine(center + forward + up, center + forward + down);
+            Gizmos.DrawLine(center + back + up, center + back + down);
+            Gizmos.DrawLine(center + right + up, center + right + down);
+            Gizmos.DrawLine(center + left + up, center + left + down);
         }
-
-        private void DrawHemisphere(Vector3 center, Vector3 right, Vector3 up, Vector3 forward)
+        private static void DrawHemisphere(Vector3 center, Vector3 right, Vector3 up, Vector3 forward, float radius)
         {
             right = right.normalized;
             up = up.normalized;
             forward = forward.normalized;
-            DrawCircle(center, forward, right, Query.Radius);
-            DrawHalfCircle(center, forward, up, Query.Radius);
-            DrawHalfCircle(center, right, up, Query.Radius);
+            DrawCircle(center, forward, right, radius);
+            DrawHalfCircle(center, forward, up, radius);
+            DrawHalfCircle(center, right, up, radius);
         }
-        private void DrawHalfCircle(Vector3 center, Vector3 xAxis, Vector3 yAxis, float radius)
+        private static void DrawHalfCircle(Vector3 center, Vector3 xAxis, Vector3 yAxis, float radius)
         {
             DrawArc(center, xAxis, yAxis, radius, Mathf.PI, LineSegmentsPerCircle / 2);
         }
-        private void DrawCircle(Vector3 center, Vector3 xAxis, Vector3 yAxis, float radius)
+        private static void DrawCircle(Vector3 center, Vector3 xAxis, Vector3 yAxis, float radius)
         {
             DrawArc(center, xAxis, yAxis, radius, Mathf.PI * 2, LineSegmentsPerCircle);
         }
-        private void DrawArc(Vector3 center, Vector3 xAxis, Vector3 yAxis, float radius, float maxAngle, int segmentCount)
+        private static void DrawArc(Vector3 center, Vector3 xAxis, Vector3 yAxis, float radius, float maxAngle, int segmentCount)
         {
             for (int i = 0; i < segmentCount; i++)
             {
                 DrawArcSegment(center, xAxis, yAxis, radius, maxAngle, i, segmentCount);
             }
         }
-        private void DrawArcSegment(Vector3 center, Vector3 xAxis, Vector3 yAxis, float radius, float maxAngle, int segment, int segmentCount)
+        private static void DrawArcSegment(Vector3 center, Vector3 xAxis, Vector3 yAxis, float radius, float maxAngle, int segment, int segmentCount)
         {
             Vector3 position1 = GetPositionOnArc(center, xAxis, yAxis, radius, maxAngle, segment, segmentCount);
             Vector3 position2 = GetPositionOnArc(center, xAxis, yAxis, radius, maxAngle, segment + 1, segmentCount);
             Gizmos.DrawLine(position1, position2);
         }
-        private Vector3 GetPositionOnArc(Vector3 center, Vector3 xAxis, Vector3 yAxis, float radius, float maxAngle, int position, int segmentCount)
+        private static Vector3 GetPositionOnArc(Vector3 center, Vector3 xAxis, Vector3 yAxis, float radius, float maxAngle, int position, int segmentCount)
         {
             float proportion = (float)position / segmentCount;
             float angle = maxAngle * proportion;
@@ -76,11 +80,11 @@ namespace PhysicsQuery
             float y = Mathf.Sin(angle) * radius;
             return center + xAxis * x + yAxis * y;
         }
-        private Vector3 GetArbitraryCrossAxis(Vector3 initial)
+        private static Vector3 GetArbitraryCrossAxis(Vector3 initial)
         {
             return Colinear(initial, Vector3.up) ? Vector3.right : Vector3.up;
         }
-        private bool Colinear(Vector3 a, Vector3 b)
+        private static bool Colinear(Vector3 a, Vector3 b)
         {
             float angle = Vector3.Angle(a, b);
             return angle < 0.01f || angle > 179.99f;
