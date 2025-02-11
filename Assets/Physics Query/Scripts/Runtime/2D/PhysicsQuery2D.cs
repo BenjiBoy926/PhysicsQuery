90a0df308c097b1bf2f0ba65a3bb63a3d7480b7d
Codey Huntting <creativecodey@gmail.com>
2025-02-11 16:29:11 -0600

    added 2D query

---
+++ Assets/Physics Query/Scripts/Runtime/2D/PhysicsQuery2D.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace PQuery
+{
+    public class PhysicsQuery2D : PhysicsQueryGeneric<VectorWrapper2D, RayWrapper2D, RaycastHit2D, Collider2D, ResultSort2D, PhysicsShape2D>
+    {
+
+    }
+}
