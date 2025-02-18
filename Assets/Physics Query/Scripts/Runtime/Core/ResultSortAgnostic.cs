e009d33cb97dc7c7fab009809b2576a2e79cad88
Codey Huntting <creativecodey@gmail.com>
2025-02-18 15:31:28 -0600

    another rename for consistent terms

--- Assets/Physics Query/Scripts/Runtime/Core/ResultSortAgnostic.cs
+++ Assets/Physics Query/Scripts/Runtime/Core/ResultSortAgnostic.cs
@@ -2,14 +2,14 @@
 {
     public static class ResultSortAgnostic
     {
-        public static readonly ResultSort<AgnosticRaycastHit> None = new ResultSortMinimal_None();
-        public static readonly ResultSort<AgnosticRaycastHit> Distance = new ResultSortMinimal_Distance();
+        public static readonly ResultSort<AgnosticRaycastHit> None = new ResultSortAgnostic_None();
+        public static readonly ResultSort<AgnosticRaycastHit> Distance = new ResultSortAgnostic_Distance();
     }
-    public class ResultSortMinimal_None : ResultSort_None<AgnosticRaycastHit>
+    public class ResultSortAgnostic_None : ResultSort_None<AgnosticRaycastHit>
     {
 
     }
-    public class ResultSortMinimal_Distance : ResultSort_Distance<AgnosticRaycastHit>
+    public class ResultSortAgnostic_Distance : ResultSort_Distance<AgnosticRaycastHit>
     {
         protected override float Distance(AgnosticRaycastHit hit)
         {
